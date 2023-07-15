72fafad7eee89d178ddbf00119a5d9b4478622d4
J-Verz <71394762+J-Verz@users.noreply.github.com>
2023-07-15 23:24:00 +0200

    Refactor code to separate modules

--- 05/part-a.rb
+++ 05/part-a.rb
@@ -1,116 +1,18 @@
-require './shared'
-require 'pry-rescue'
+require "./shared"
+require "pry-rescue"
 
 class PartA
-    def self.run
-        # Get input and parse to data model
-        self.parse_input InputReader.real
-        # Execute instructions
-        self.execute_instructions
-        # Tell elves which crates are on top
-        self.find_top_crates
+  extend Shared::InputParser
+  extend Shared::OutputWriter
+  extend Shared::ChallengeExecutor
+
+  def self.execute_instructions
+    @instructions.each do |instruction|
+      instruction.amount.times do
+        @stacks[instruction.to].push(@stacks[instruction.from].pop)
+      end
     end
-
-    def self.parse_input(input)
-        self.initial_configuration(input).each do |line|
-            case line
-            when /(\s+[0-9])+/
-                self.create_stacks line
-            when /\s+(\[[A-Z]\])+\s+/
-                self.create_crates_on_stack line
-            else
-                raise "The following line is invalid:\n'#{line}'"
-            end
-        end
-
-        @@instructions = self.instructions(input).map do |line|
-            ::Instruction.new line
-        end
-        
-    end
-
-    def self.execute_instructions
-        @@instructions.each do |instruction|
-            instruction.amount.times do
-                @@stacks[instruction.to].push(@@stacks[instruction.from].pop)
-            end
-        end
-    end
-
-    def self.find_top_crates
-        @@stacks.each_value.map {|stack| stack.top.identifier }.join
-    end
-
-    private
-
-    def self.create_stacks(line)
-        @@stacks = {}
-        line.scan(/[0-9]/) do |number|
-            @@stacks[number.to_i] = ::StackOfCrates.new number.to_i
-        end
-    end
-
-    def self.create_crates_on_stack(line)
-        @@stacks.length.times do |i|
-            line_piece = line[i*4..i*4+3]
-            next unless line_piece.include? "["
-            line_piece.scan(/[A-Z]/) do |crate_id|
-                @@stacks[i+1].push ::Crate.new crate_id
-            end
-        end
-    end
-
-    def self.split_point(input)
-        input.find_index {|i| i == "" }
-    end
-
-    def self.initial_configuration(input)
-        input[0..self.split_point(input)-1].reverse!
-    end
-
-    def self.instructions(input)
-        input[self.split_point(input)+1..-1]
-    end
+  end
 end
 
-class Instruction
-    attr_reader :amount, :from, :to, :full
-
-    def initialize(complete_instruction)
-        @full = complete_instruction
-        numbers = complete_instruction.scan(/[0-9]+/).map(&:to_i)
-        
-        @amount = numbers[0]
-        @from = numbers[1]
-        @to = numbers[2]
-    end
-end
-
-class Crate
-    attr_reader :identifier
-
-    def initialize(identifier)
-        @identifier = identifier
-    end
-end
-
-class StackOfCrates
-    def initialize(number)
-        @number = number
-        @crates = []
-    end
-
-    def push(crate)
-        @crates.push crate
-    end
-
-    def pop
-        @crates.pop
-    end
-
-    def top
-        @crates.last
-    end
-end
-
-puts PartA.run
+PartA.run
--- 05/shared.rb
+++ 05/shared.rb
@@ -1,2 +1,110 @@
 require "../common"
 
+module Shared
+  module ChallengeExecutor
+    def run
+      # Get input and parse to data model
+      self.parse_input InputReader.example
+      # Execute instructions
+      self.execute_instructions
+      # Tell elves which crates are on top
+      puts self.find_top_crates
+    end
+  end
+
+  module InputParser
+    def parse_input(input)
+      initial_configuration(input).each do |line|
+        case line
+        when /(\s+[0-9])+/
+          create_stacks line
+        when /\s+(\[[A-Z]\])+\s+/
+          create_crates_on_stack line
+        else
+          raise "The following line is invalid:\n'#{line}'"
+        end
+      end
+
+      @instructions = instructions(input).map do |line|
+        Instruction.new line
+      end
+    end
+
+    private
+
+    def create_stacks(line)
+      @stacks = {}
+      line.scan(/[0-9]/) do |number|
+        @stacks[number.to_i] = StackOfCrates.new number.to_i
+      end
+    end
+
+    def create_crates_on_stack(line)
+      @stacks.length.times do |i|
+        line_piece = line[i * 4..i * 4 + 3]
+        next unless line_piece.include? "["
+        line_piece.scan(/[A-Z]/) do |crate_id|
+          @stacks[i + 1].push Crate.new crate_id
+        end
+      end
+    end
+
+    def split_point(input)
+      input.find_index { |i| i == "" }
+    end
+
+    def initial_configuration(input)
+      input[0..split_point(input) - 1].reverse!
+    end
+
+    def instructions(input)
+      input[split_point(input) + 1..-1]
+    end
+  end
+
+  module OutputWriter
+    def find_top_crates
+      @stacks.each_value.map { |stack| stack.top.identifier }.join
+    end
+  end
+
+  class Instruction
+    attr_reader :amount, :from, :to, :full
+
+    def initialize(complete_instruction)
+      @full = complete_instruction
+      numbers = complete_instruction.scan(/[0-9]+/).map(&:to_i)
+
+      @amount = numbers[0]
+      @from = numbers[1]
+      @to = numbers[2]
+    end
+  end
+
+  class Crate
+    attr_reader :identifier
+
+    def initialize(identifier)
+      @identifier = identifier
+    end
+  end
+
+  class StackOfCrates
+    def initialize(number)
+      @number = number
+      @crates = []
+    end
+
+    def push(crate)
+      @crates.push crate
+    end
+
+    def pop
+      @crates.pop
+    end
+
+    def top
+      @crates.last
+    end
+  end
+end
