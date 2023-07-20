b6d4e37b64b66b065d334fa8041f6a9f528e9e7c
J-Verz <71394762+J-Verz@users.noreply.github.com>
2023-07-20 23:05:33 +0200

    Move input scanning to seperate class to reuse in part b

--- 07/part-a.rb
+++ 07/part-a.rb
@@ -3,37 +3,8 @@ require "./shared"
 class PartA
   def self.run
     input = Common::InputReader.real
-    self.scan_input input
+    @root = Shared::DirectoryTreeParser.parse input
     puts self.sum_of_sizes_under_100_001
-  end
-
-  def self.scan_input(input)
-    input.each do |line|
-      case line
-      when /^\$\scd\s(?<name>.+)$/
-        folder_name = $~[:name]
-        case folder_name
-        when "/" # if name is '/', create Root
-          @current_folder = @root = Shared::Root.new
-        when ".." # if name is '..', move to parent folder
-          @current_folder = @current_folder.parent
-        else # else, change context to specified folder
-          @current_folder = @current_folder.child folder_name
-        end
-      when /^\$\sls$/
-        # listing folder inputs, read to create content.
-        # Doesn't have to do anything
-      when /^(?<size>\d+)\s(?<name>.+)$/
-        file_size = $~[:size].to_i
-        file_name = $~[:name]
-        @current_folder.add_child Shared::File.new(file_name, file_size)
-      when /^dir\s(?<name>.+)$/
-        folder_name = $~[:name]
-        Shared::Folder.new(folder_name, @current_folder)
-      else
-        raise "This line,\n'#{line}'\nis not recognized."
-      end
-    end
   end
 
   def self.sum_of_sizes_under_100_001
--- 07/shared.rb
+++ 07/shared.rb
@@ -1,6 +1,38 @@
 require "../common"
 
 module Shared
+  class DirectoryTreeParser
+    def self.parse(input)
+      input.each do |line|
+        case line
+        when /^\$\scd\s(?<name>.+)$/
+          folder_name = $~[:name]
+          case folder_name
+          when "/" # if name is '/', create Root
+            @current_folder = @root = Shared::Root.new
+          when ".." # if name is '..', move to parent folder
+            @current_folder = @current_folder.parent
+          else # else, change context to specified folder
+            @current_folder = @current_folder.child folder_name
+          end
+        when /^\$\sls$/
+          # listing folder inputs, read to create content.
+          # Doesn't have to do anything
+        when /^(?<size>\d+)\s(?<name>.+)$/
+          file_size = $~[:size].to_i
+          file_name = $~[:name]
+          @current_folder.add_child Shared::File.new(file_name, file_size)
+        when /^dir\s(?<name>.+)$/
+          folder_name = $~[:name]
+          Shared::Folder.new(folder_name, @current_folder)
+        else
+          raise "This line,\n'#{line}'\nis not recognized."
+        end
+      end
+      @root
+    end
+  end
+
   class Folder
     attr_reader :parent, :name
 
