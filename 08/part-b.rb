268f6a137c3f5da2435a63454e3cbfc57e2bf765
J-Verz <71394762+J-Verz@users.noreply.github.com>
2023-08-22 16:57:54 +0200

    Refactor PartB to separate Module

--- 08/part-b.rb
+++ 08/part-b.rb
@@ -1,64 +1,19 @@
 require './shared'
 
 class PartB
+  extend Shared::ScenicScoreCalculator
+
   def self.run
     input = Common::InputReader.real
     @grid = Shared::TreeGrid.new(input)
     puts self.find_best_scenic_score
   end
+
+  private
   
   def self.find_best_scenic_score
-    @grid.size[:x].times do |x|
-      @grid.size[:y].times do |y|
-        view_point = {x: x, y: y}
-        @grid[view_point].scenic_score = 
-          self.look_right_from(view_point) *
-          self.look_left_from(view_point) *
-          self.look_down_from(view_point) *
-          self.look_up_from(view_point)
-      end
-    end
-    @grid.best_scenic_score
-  end
-
-  def self.look_up_from(view_point)
-    neighbour = view_point.dup
-    while neighbour[:y] > 0
-      neighbour[:y] -= 1
-      break if self.check_if_neighbour_is_smaller(view_point, neighbour)
-    end
-    view_point[:y] - neighbour[:y]
-  end
-
-  def self.look_left_from(view_point)
-    neighbour = view_point.dup
-    while neighbour[:x] > 0
-      neighbour[:x] -= 1
-      break if self.check_if_neighbour_is_smaller(view_point, neighbour)
-    end
-    view_point[:x] - neighbour[:x]
-  end
-
-  def self.look_right_from(view_point)
-    neighbour = view_point.dup
-    while neighbour[:x] < @grid.size[:x]-1
-      neighbour[:x] += 1
-      break if self.check_if_neighbour_is_smaller(view_point, neighbour)
-    end
-    neighbour[:x] - view_point[:x]
-  end
-
-  def self.look_down_from(view_point)
-    neighbour = view_point.dup
-    while neighbour[:y] < @grid.size[:y]-1
-      neighbour[:y] += 1
-      break if self.check_if_neighbour_is_smaller(view_point, neighbour)
-    end
-    neighbour[:y] - view_point[:y]
-  end
-
-  def self.check_if_neighbour_is_smaller(we, neighbour)
-    @grid[we].height <= @grid[neighbour].height
+    self.calculate_scenic_scores
+    @grid.find_best_scenic_score
   end
 end
 
--- 08/shared.rb
+++ 08/shared.rb
@@ -4,18 +4,18 @@ require "pry-rescue"
 module Shared
   module TreeGridInspector
     private
-    
+
     def look_from(side)
       direction = xy_to_dw_mapping side
-      
+
       width_enumerator(side, direction[:w]) do |w|
         biggest_height_so_far = -1
-        
+
         depth_enumerator(side, direction[:d]) do |d|
           tree_currently_observing = @grid[map_dw_to_xy(side: side, d: d, w: w)]
-          
+
           next if tree_currently_observing.height <= biggest_height_so_far
-  
+
           tree_currently_observing.is_visible
           biggest_height_so_far = tree_currently_observing.height
         end
@@ -30,7 +30,7 @@ module Shared
         { x: d, y: w }
       end
     end
-  
+
     def xy_to_dw_mapping(side)
       case side
       when :above, :below
@@ -39,11 +39,11 @@ module Shared
         { w: :y, d: :x }
       end
     end
-  
+
     def width_enumerator(side, direction, &block)
       @grid.size[direction].times &block
     end
-  
+
     def depth_enumerator(side, direction, &block)
       enumerator = @grid.size[direction].times
       case side
@@ -52,6 +52,64 @@ module Shared
       when :below, :right
         enumerator.reverse_each &block
       end
+    end
+  end
+
+  module ScenicScoreCalculator
+    def calculate_scenic_scores
+      @grid.size[:x].times do |x|
+        @grid.size[:y].times do |y|
+          self.calculate_scenic_score_for x: x, y: y
+        end
+      end
+    end
+
+    def calculate_scenic_score_for(point)
+      @grid[point].scenic_score =
+        self.look_right_from(point) *
+        self.look_left_from(point) *
+        self.look_down_from(point) *
+        self.look_up_from(point)
+    end
+
+    def look_up_from(view_point)
+      neighbour = view_point.dup
+      while neighbour[:y] > 0
+        neighbour[:y] -= 1
+        break if self.check_if_neighbour_is_smaller(view_point, neighbour)
+      end
+      view_point[:y] - neighbour[:y]
+    end
+
+    def look_left_from(view_point)
+      neighbour = view_point.dup
+      while neighbour[:x] > 0
+        neighbour[:x] -= 1
+        break if self.check_if_neighbour_is_smaller(view_point, neighbour)
+      end
+      view_point[:x] - neighbour[:x]
+    end
+
+    def look_right_from(view_point)
+      neighbour = view_point.dup
+      while neighbour[:x] < @grid.size[:x] - 1
+        neighbour[:x] += 1
+        break if self.check_if_neighbour_is_smaller(view_point, neighbour)
+      end
+      neighbour[:x] - view_point[:x]
+    end
+
+    def look_down_from(view_point)
+      neighbour = view_point.dup
+      while neighbour[:y] < @grid.size[:y] - 1
+        neighbour[:y] += 1
+        break if self.check_if_neighbour_is_smaller(view_point, neighbour)
+      end
+      neighbour[:y] - view_point[:y]
+    end
+
+    def check_if_neighbour_is_smaller(we, neighbour)
+      @grid[we].height <= @grid[neighbour].height
     end
   end
 
@@ -88,7 +146,7 @@ module Shared
       @grid.flatten.filter_map { |tree| tree.is_visible? }.size
     end
 
-    def best_scenic_score
+    def find_best_scenic_score
       @grid.flatten.map(&:scenic_score).max
     end
 
