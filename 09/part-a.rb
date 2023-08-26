86c59797c0d67853201befc8df85756b6814ab43
J-Verz <71394762+J-Verz@users.noreply.github.com>
2023-08-26 08:35:51 +0200

    Scaffold day 09

---
+++ 09/part-a.rb
@@ -0,0 +1,16 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+require './shared'
+
+class PartA
+  def self.run
+    input = Common::InputReader.example
+
+  end
+
+  private
+end
+
+if __FILE__ == $0
+  PartA.run
+end
---
+++ 09/shared.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+require '../common'
+require 'pry-rescue'
+
+module Shared
+
+end
