1185792854ea4ae145aa1e1a004675457e0c65ce
J-Verz <71394762+J-Verz@users.noreply.github.com>
2023-08-27 09:22:22 +0200

    Solve day 9 part a

--- 09/part-a.rb
+++ 09/part-a.rb
@@ -1,14 +1,43 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
-require './shared'
+require "./shared"
+
+def debug(*values)
+  puts values if ARGV[0] == "--debug"
+end
 
 class PartA
-  def self.run
-    input = Common::InputReader.example
+  attr_reader :head, :tail
+  DIRECTION_TO_METHOD = {
+    "U" => :up,
+    "D" => :down,
+    "R" => :right,
+    "L" => :left,
+  }
 
+  def self.run
+    input = self.parse_input Common::InputReader.real
+    head = Shared::RopeHead.new
+    tail = Shared::RopeTail.new
+    input.each do |movement|
+      movement[1].to_i.times do
+        head.send movement[0]
+        tail.follow head
+        debug "HEAD #{head.position}", "TAIL #{tail.position}"
+      end
+    end
+    puts tail.unique_visited_coordinates
   end
 
   private
+
+  def self.parse_input(input)
+    input.map do |line|
+      line.split(' ').tap do |instruction|
+        instruction[0] = DIRECTION_TO_METHOD.dig(instruction[0])
+      end
+    end
+  end
 end
 
 if __FILE__ == $0
--- 09/shared.rb
+++ 09/shared.rb
@@ -3,5 +3,116 @@ require '../common'
 require 'pry-rescue'
 
 module Shared
-
+  class Coords
+    attr_accessor :x, :y
+  
+    def initialize(x: 0, y: 0)
+      @x = x
+      @y = y
+    end
+  
+    def -(other)
+      Coords.new x: self.x - other.x, y: self.y - other.y
+    end
+  
+    def to_h
+      { x: @x, y: @y }
+    end
+  
+    def inspect
+      self.to_h.inspect
+    end
+  
+    def to_s
+      inspect
+    end
+  end
+  
+  class RopeEnd
+    attr_reader :position
+  
+    def initialize
+      @position = Coords.new
+    end
+  
+    def name
+      self.class.to_s
+    end
+  
+    def up
+      debug "#{self.name} going up"
+      @position.y += 1
+      self
+    end
+  
+    def down
+      debug "#{self.name} going down"
+      @position.y -= 1
+      self
+    end
+  
+    def right
+      debug "#{self.name} going right"
+      @position.x += 1
+      self
+    end
+  
+    def left
+      debug "#{self.name} going left"
+      @position.x -= 1
+      self
+    end
+  end
+  
+  class RopeHead < RopeEnd; end
+  
+  class RopeTail < RopeEnd
+    attr_reader :visited_coordinates
+  
+    def initialize
+      super
+      @visited_coordinates = Hash.new do |hash, key|
+        hash[key] = Hash.new do |hash, key|
+          hash[key] = true
+        end
+      end
+      @visited_coordinates[0][0]
+      # binding.pry
+    end
+  
+    def follow(head)
+      distance_to_head = (self.position - head.position).to_h
+      case distance_to_head
+      when { x: 0, y: -2 }
+        self.up
+      when { x: 0, y: 2 }
+        self.down
+      when { x: -2, y: 0 }
+        self.right
+      when { x: 2, y: 0 }
+        self.left
+      when { x: -1, y: -2 }
+        self.up.right
+      when { x: 1, y: -2 }
+        self.up.left
+      when { x: -1, y: 2 }
+        self.down.right
+      when { x: 1, y: 2 }
+        self.down.left
+      when { x: -2, y: -1 }
+        self.up.right
+      when { x: 2, y: -1 }
+        self.up.left
+      when { x: -2, y: 1 }
+        self.down.right
+      when { x: 2, y: 1 }
+        self.down.left
+      end
+      @visited_coordinates[self.position.x][self.position.y]
+    end
+  
+    def unique_visited_coordinates
+      @visited_coordinates.map { |key, value| value.size }.sum
+    end
+  end
 end
