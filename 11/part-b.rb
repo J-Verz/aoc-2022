57fa3b97b2c170809479b66d895aa46710ebef3d
J-Verz <71394762+J-Verz@users.noreply.github.com>
2023-11-04 15:12:31 +0100

    Start on day 11

---
+++ 11/part-b.rb
@@ -0,0 +1,16 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+require_relative './shared'
+
+class PartB
+  def self.run
+    input = Common::InputReader.example
+
+  end
+
+  private
+end
+
+if __FILE__ == $0
+  PartB.run
+end
---
+++ 11/shared.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+require_relative '../common'
+require 'pry-rescue'
+
+DAY = "11"
+
+module Shared
+
+end
